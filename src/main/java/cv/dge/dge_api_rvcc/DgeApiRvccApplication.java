b0a49d214a4d22a1f93c64a076ea212ae4c9bdd4
janir <janiralves10@gmail.com>
2026-03-18 17:21:09 -0100

    fix

---
+++ src/main/java/cv/dge/dge_api_rvcc/DgeApiRvccApplication.java
@@ -0,0 +1,13 @@
+package cv.dge.dge_api_rvcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgeApiRvccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgeApiRvccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cv/dge/dge_api_rvcc/DgeApiRvccApplicationTests.java
@@ -0,0 +1,13 @@
+package cv.dge.dge_api_rvcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgeApiRvccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
